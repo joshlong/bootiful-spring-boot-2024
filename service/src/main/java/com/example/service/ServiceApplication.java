5e3d55334f56e3df2a6b44542c11d411b666b7ca
Josh Long <josh@joshlong.com>
2024-05-07 09:15:54 +0200

    ui

--- service/src/main/java/com/example/service/ServiceApplication.java
+++ service/src/main/java/com/example/service/ServiceApplication.java
@@ -2,15 +2,7 @@ package com.example.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.Map;
-
-@Controller
-@ResponseBody
 @SpringBootApplication
 public class ServiceApplication {
 
